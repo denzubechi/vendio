5c060852ab205b12c8ca5c37be27f5ddaae0f9f2
Samuel Chukwuma <98186867+denzubechi@users.noreply.github.com>
2025-08-19 13:16:10 +0100

    Update route.ts

--- app/api/dashboard/customers/route.ts
+++ app/api/dashboard/customers/route.ts
@@ -23,7 +23,7 @@ export async function GET(request: NextRequest) {
     }
 
     const orders = await prisma.order.findMany({
-      where: { sellerId: userId },
+      where: { sellerId: user.id },
       include: {
         items: {
           include: {
