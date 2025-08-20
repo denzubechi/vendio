62d1d604a53f8e6efeadcbaba3600f90c9865a0a
denzubechi <chukwumasamuel371@gmail.com>
2025-08-20 01:25:14 +0100

    fix:preview display:

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -34,7 +34,6 @@ export async function GET() {
       ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
-      noindex: "true",
     }),
   });
 }
