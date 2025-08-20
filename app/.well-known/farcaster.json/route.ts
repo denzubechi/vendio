49cd25c266147ede5a796d4e1853a21aea1b2d5f
denzubechi <chukwumasamuel371@gmail.com>
2025-08-20 01:27:03 +0100

    fix:preview display:

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -28,7 +28,7 @@ export async function GET() {
       homeUrl: URL,
       webhookUrl: `${URL}/api/webhook`,
       primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
-      tags: ["fun", "social", "fast"],
+      tags: ["fun", "social", "fast", "miniapp", "baseapp"],
       heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
       tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
       ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
